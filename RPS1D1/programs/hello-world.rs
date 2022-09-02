521e9659f68f102d027cf71f0e409bc4402ce27e
Bobbbay <abatterysingle@gmail.com>
2022-09-02 03:29:50 +0000

    feat: introduction presentation

--- RPS1D1/programs/hello-world.rs
+++ RPS1D1/programs/hello-world.rs
@@ -1,5 +1,7 @@
 fn main() {
+    // This is a comment. The computer fully ignores this,
+    // so we can use it to describe code to other humans,
+    // if necessary.
+
     println!("Hello, world!");
 }
-
-// More content.
